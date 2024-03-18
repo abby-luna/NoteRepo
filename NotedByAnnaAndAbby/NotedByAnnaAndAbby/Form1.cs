9e0d111ee2ead5eedcfcfde6a142eda24c25f542
Abigail Garvey <74800099+abby-luna@users.noreply.github.com>
2024-03-18 12:23:43 +0000

    Created base class for note

--- NotedByAnnaAndAbby/NotedByAnnaAndAbby/Form1.cs
+++ NotedByAnnaAndAbby/NotedByAnnaAndAbby/Form1.cs
@@ -12,9 +12,29 @@ namespace NotedByAnnaAndAbby
 {
     public partial class Form1 : Form
     {
+
+        List<Note> notes = new List<Note>();
         public Form1()
         {
             InitializeComponent();
+
+            notes.Add(new Note(1, "Hello", "World"));
+            notes.Add(new Note(2, "e", "World"));
+            notes.Add(new Note(3, "q", "World"));
+            notes.Add(new Note(4, "e", "World"));
+            notes.Add(new Note(5, "w", "World"));
+            notes.Add(new Note(6, "r", "World"));
+            notes.Add(new Note(7, "t", "World"));
+            notes.Add(new Note(8, "b", "World"));
+
+
+            dataGridView1.DataSource = notes;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
---
+++ NotedByAnnaAndAbby/NotedByAnnaAndAbby/Note.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotedByAnnaAndAbby
+{
+    internal class Note
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        protected string Content {  get; set; }
+
+
+        public Note(int id, string title, string content)
+        {
+            Id = id;
+            Title = title;
+            Content = content;
+        }
+    }
+}
